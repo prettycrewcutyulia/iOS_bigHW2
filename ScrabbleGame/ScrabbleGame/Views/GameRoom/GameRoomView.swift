964f63b2bdf4bc29d2642b258a0b30c744685d56
irina <irinapechik@gmail.com>
2024-06-12 21:57:14 +0300

    added game settings to game room

--- ScrabbleGame/ScrabbleGame/Views/GameRoom/GameRoomView.swift
+++ ScrabbleGame/ScrabbleGame/Views/GameRoom/GameRoomView.swift
@@ -9,12 +9,91 @@ import SwiftUI
 
 struct GameRoomView: View {
     @Binding var gameRoom: GameRoom
+    @State var leaveRoom: Bool = false
+    
+    @State var buttonText: String = GameButtonText.StartGame.rawValue
+    @State var buttonColor: Color = .green
     
     var body: some View {
-        Text("\(gameRoom.adminNickname), \(gameRoom.currentNumberOfChips), \(gameRoom.gameStatus), \(gameRoom.roomCode)")
+        NavigationStack {
+            GameTopBar(gameRoom: $gameRoom, leaveRoom: $leaveRoom)
+            Spacer()
+            ButtonView(buttonText: $buttonText, buttonColor:  $buttonColor, isDisabled: false) {
+                changeGameStatus(buttonText: buttonText)
+                // TODO: записать в бд статус комнаты
+            }
+            Spacer()
+        }
+    }
+    
+    func changeGameStatus(buttonText: String) {
+        if buttonText == GameButtonText.StartGame.rawValue {
+            self.buttonText = GameButtonText.PauseGame.rawValue
+            self.buttonColor = .gray
+            // Менять статус игры
+        } else {
+            self.buttonText = GameButtonText.StartGame.rawValue
+            self.buttonColor = .green
+            // Менять статус игры
+
+        }
+    }
+}
+
+enum GameButtonText: String {
+    case StartGame = "Start game"
+    case PauseGame = "Pause game"
+}
+
+struct GameTopBar: View {
+    @Binding var gameRoom: GameRoom
+    @Binding var leaveRoom: Bool
+    
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Game status:")
+                                .bold()
+                            Text("\(gameRoom.gameStatus)")
+                                .foregroundStyle(gameRoom.gameStatus == "Not started" ? .gray : gameRoom.gameStatus == "Started" ? .green : .red)
+                        }
+                        HStack {
+                            Text("Room admin:")
+                                .bold()
+                            Text("\(gameRoom.adminNickname)")
+                        }
+                    }
+                    .padding()
+                    Spacer()
+                    HStack {
+                        Text("\(gameRoom.currentNumberOfChips)")
+                            .foregroundStyle(gameRoom.currentNumberOfChips == 0 ? .red : .green)
+                        Image(systemName: "dollarsign.circle")
+                            .foregroundStyle(.yellow)
+                        
+                    }
+                    Spacer()
+                    Button {
+                        // TODO: leave room
+                    } label: {
+                        Image(systemName: "arrowshape.turn.up.right")
+                            .foregroundStyle(.black)
+                    }
+                    Spacer()
+                    
+                }
+            }
+        }
+        .fullScreenCover(isPresented: $leaveRoom) {
+            // TODO: Переход на другой экран всех комнат
+            Text("You lived room")
+        }
     }
 }
 
 #Preview {
-    GameRoomView(gameRoom: Binding<GameRoom>.constant(GameRoom(id: "gameID", adminNickname: "adminNickName", roomCode: "roomCode", gameStatus: "gamestatus", currentNumberOfChips: 0)))
+    GameRoomView(gameRoom: Binding<GameRoom>.constant(GameRoom(id: "gameID", adminNickname: "adminNickName", roomCode: "roomCode", gameStatus: "Not started", currentNumberOfChips: 0)))
 }
