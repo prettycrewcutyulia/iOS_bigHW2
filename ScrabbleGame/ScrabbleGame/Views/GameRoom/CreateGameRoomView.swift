3242b9abde49bd6aca78f06cdc541140af164235
irina <irinapechik@gmail.com>
2024-06-12 21:56:50 +0300

    added toggles for private&public room creation

--- ScrabbleGame/ScrabbleGame/Views/GameRoom/CreateGameRoomView.swift
+++ ScrabbleGame/ScrabbleGame/Views/GameRoom/CreateGameRoomView.swift
@@ -11,8 +11,12 @@ struct CreateGameRoomView: View {
     @Binding var adminNickname: String
     @State var roomCode: String = ""
     @State var gameRoom: GameRoom? = nil
+    
     @State var showGameRoom: Bool = false
     @State var showErrorAlert: Bool = false
+    @State var isRoomPrivate: Bool = false
+    @State var isRoomPublic: Bool = false
+
     
     var body: some View {
         NavigationStack {
@@ -24,11 +28,47 @@ struct CreateGameRoomView: View {
                 
                 Spacer()
                 
-                CustomTextView(variable: $roomCode, placeholderName: "Enter room code")
+                Toggle(isOn: $isRoomPublic) {
+                    Text("Create public room")
+                }
+                .frame(width: 340, height: 61)
+                .toggleStyle(SwitchToggleStyle(tint: .black))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(isRoomPublic ? .black : .gray, lineWidth: isRoomPublic ? 0.5 : 0.2)
+                        .frame(width: 370)
+                )
+                .onChange(of: isRoomPublic) {
+                    if isRoomPublic {
+                        isRoomPrivate = false
+                    }
+                }
+                .padding(.bottom, 20)
+                
+                Toggle(isOn: $isRoomPrivate) {
+                    Text("Create private room")
+                }
+                .frame(width: 340, height: 61)
+                .toggleStyle(SwitchToggleStyle(tint: .black))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(isRoomPrivate ? .black : .gray, lineWidth: isRoomPrivate ? 0.5 : 0.2)
+                        .frame(width: 370)
+                )
+                .onChange(of: isRoomPrivate) {
+                    if isRoomPrivate {
+                        isRoomPublic = false
+                    }
+                }
+                .padding(.bottom, 20)
+                
+                if isRoomPrivate {
+                    CustomTextView(variable: $roomCode, placeholderName: "Enter room code")
+                }
                 
                 Spacer()
                 
-                NextButtonView(isDisabled: roomCode.isEmpty) {
+                ButtonView(buttonText: Binding<String>.constant("Next"), buttonColor: Binding<Color>.constant(.black), isDisabled: isRoomPublic ? false : roomCode.isEmpty) {
                     // запрос
                     Task {
                         do {
