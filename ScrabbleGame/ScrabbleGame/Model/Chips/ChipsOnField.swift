ee24599fe2a3e87344341cddafa87496ef1b0452
YaroslavGamayunov <yaroslav.gamayunov@gmail.com>
2024-06-16 07:22:52 +0300

    Add chip field

---
+++ ScrabbleGame/ScrabbleGame/Model/Chips/ChipsOnField.swift
@@ -0,0 +1,18 @@
+//
+//  ChipsOnField.swift
+//  ScrabbleGame
+//
+//  Created by Ярослав Гамаюнов on 16.06.2024.
+//
+
+import Foundation
+
+// Фишки на поле
+struct ChipsOnField: Identifiable, Codable {
+    let id: UUID?
+    var coordinate: Coordinate
+    var chip: Chip
+    var isSelected: Bool
+}
+
+
---
+++ ScrabbleGame/ScrabbleGame/Model/Chips/Coordinate.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinate.swift
+//  ScrabbleGame
+//
+//  Created by Ярослав Гамаюнов on 16.06.2024.
+//
+
+import Foundation
+
+// Координата
+struct Coordinate: Codable, Hashable {
+    let x: String;
+    let y: Int;
+}
